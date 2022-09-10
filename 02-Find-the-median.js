a3c9f8bf5babec1db28628ccdd862e5ee5a406ff
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-10 17:47:43 -0500

    Exc-2 Definition

---
+++ 02-Find-the-median.js
@@ -0,0 +1,16 @@
+/*
+Excercise 2
+https://www.hackerrank.com/challenges/find-the-median/problem?h_r=next-challenge&h_v=zen
+*/
+
+/*
+ * Complete the 'findMedian' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function findMedian(arr) {
+    // Write your code here
+
+}
