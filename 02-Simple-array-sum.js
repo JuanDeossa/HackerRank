87bf4cb6b9dc7591db510e696ac1a175b2266db1
JuanDeossa <juandeossa.nc@gmail.com>
2022-09-10 18:09:27 -0500

    Exc-2 name update

---
+++ 02-Simple-array-sum.js
@@ -0,0 +1,15 @@
+/*
+Excercise 2
+https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+*/
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+function simpleArraySum(ar) {
+    
+}
